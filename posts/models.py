e6759a71c4eea280dac81c153d5e7e5e2480173b
Елена Мещанчук <agatasawel_8@mail.ru>
2021-09-14 12:49:08 +0300

    Created models Post and Comment

---
+++ posts/models.py
@@ -0,0 +1,18 @@
+from django.db.models import *
+
+
+class Post(Model):
+    title = CharField(max_length=250)
+    author = CharField(max_length=100)
+    post_body = TextField()
+    published_date = DateTimeField(auto_now_add=True)
+
+
+class Comment(Model):
+    author = CharField(max_length=100)
+    comment_body = CharField(max_length=500)
+    create_data = DateTimeField(auto_now_add=True)
+
+
+class Author(Model):
+    pass
